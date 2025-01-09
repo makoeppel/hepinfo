fix calculation of MI

hepinfo/models/BaseModel.py
@@ -233,12 +233,12 @@ class BaseModel(BaseEstimator):
                     name=f"{name}_{i}"
                 )(hidden_layers)
             if self.quantized_position[i]:
+                last_quantized = hidden_layers
                 hidden_layers = QActivation(
                     activation_binary,
                     activity_regularizer=kernel_regularizer,
                     name=f"qact_{name}_{i}"
                 )(hidden_layers)
-                last_quantized = hidden_layers
             if drop_out > 0:
                 hidden_layers = tf.keras.layers.Dropout(drop_out)(hidden_layers)
 

hepinfo/models/BinaryMI.py
@@ -22,7 +22,7 @@ class BinaryMI(BaseModel):
                 quantized_position: list[bool] = [False, True, False],
                 batch_normalisation: bool = False,
                 activation_binary: str = 'bernoulli',
-                activation_nonbinary: str = 'sigmoid',
+                activation_nonbinary: str = 'tanh',
                 kernel_regularizer: float = 0.01,
                 drop_out: float = 0.,
                 gamma: float = 0.0,
@@ -241,10 +241,10 @@ class BinaryMI(BaseModel):
             index_qact = [i for i, x in enumerate(self.quantized_position) if x][-1]
             loss = {
                 f"t_{len(self.hidden_layers)}": self.loss,
-                f"qact_t_{index_qact}": self.mutual_information_bernoulli_loss
+                f"t_{index_qact}": self.mutual_information_bernoulli_loss
             }
-            lossWeights = {f"t_{len(self.hidden_layers)}": float(1 - self.gamma), f"qact_t_{index_qact}": float(self.gamma)}
-            metrics = {f"t_{len(self.hidden_layers)}": 'AUC' if self.last_layer_size == 1 else 'acc', f"qact_t_{index_qact}": 'acc'}
+            lossWeights = {f"t_{len(self.hidden_layers)}": float(1 - self.gamma), f"t_{index_qact}": float(self.gamma)}
+            metrics = {f"t_{len(self.hidden_layers)}": 'AUC' if self.last_layer_size == 1 else 'acc', f"t_{index_qact}": 'acc'}
         else:
             outputs = self.out
             loss = [self.loss]
@@ -303,6 +303,14 @@ class BinaryMI(BaseModel):
             Returns:
                 None
         """
+
+        # get the correct numpy type
+        x = self.convert_array_to_float(x)
+        y = self.convert_array_to_float(y)
+        s = self.convert_array_to_float(s)
+
+        # input size
+        self.input_shape = (x.shape[1], )
 
         # convert for classifier output
         y_train = tf.keras.utils.to_categorical(y_train, self.last_layer_size)

hepinfo/models/MiVAE.py
@@ -240,15 +240,16 @@ class MiVAE(BaseEstimator, keras.Model):
         with tf.GradientTape() as tape:
             if self.mi_loss:
                 z_mean, z_log_var, z, z_sample = self.encoder(x, training=True)
+                reconstruction = self.decoder(z_sample)
             else:
                 z_mean, z_log_var, z = self.encoder(x, training=True)
-            reconstruction = self.decoder(z)
-            reconstruction_loss = keras.losses.MeanSquaredError()(x, reconstruction)
+                reconstruction = self.decoder(z)
+            reconstruction_loss = (1 - self.beta_param) * keras.losses.MeanSquaredError()(x, reconstruction)
             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
             kl_loss = self.beta_param * tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
             mi_loss = 0
             if self.mi_loss:
-                mi_loss = self.mutual_information_bernoulli_loss(s, z_sample)
+                mi_loss = self.mutual_information_bernoulli_loss(s, z)
             total_loss = reconstruction_loss + kl_loss + self.gamma * mi_loss
 
         grads = tape.gradient(total_loss, self.trainable_weights)
@@ -270,15 +271,16 @@ class MiVAE(BaseEstimator, keras.Model):
         x, s = data
         if self.mi_loss:
             z_mean, z_log_var, z, z_sample = self.encoder(x, training=False)
+            reconstruction = self.decoder(z_sample)
         else:
             z_mean, z_log_var, z = self.encoder(x, training=False)
-        reconstruction = self.decoder(z)
-        reconstruction_loss = keras.losses.MeanSquaredError()(x, reconstruction)
+            reconstruction = self.decoder(z)
+        reconstruction_loss = (1 - self.beta_param) * keras.losses.MeanSquaredError()(x, reconstruction)
         kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
         kl_loss = self.beta_param * tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
         mi_loss = 0
         if self.mi_loss:
-            mi_loss = self.mutual_information_bernoulli_loss(s, z_sample)
+            mi_loss = self.mutual_information_bernoulli_loss(s, z)
         total_loss = reconstruction_loss + kl_loss + self.gamma * mi_loss
         self.total_loss_tracker.update_state(total_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
@@ -294,9 +296,10 @@ class MiVAE(BaseEstimator, keras.Model):
     def call(self, x, training=True):
         if self.mi_loss:
             z_mean, z_log_var, z, z_sample = self.encoder(x, training)
+            reconstruction = self.decoder(z_sample)
         else:
             z_mean, z_log_var, z = self.encoder(x, training)
-        reconstruction = self.decoder(z)
+            reconstruction = self.decoder(z)
         return reconstruction
 
     def get_encoder(self):
@@ -463,15 +466,16 @@ class MiVAE(BaseEstimator, keras.Model):
     def score(self, x, y):
         if self.mi_loss:
             z_mean, z_log_var, z, z_sample = self.encoder(x, training=False)
+            reconstruction = self.decoder(z_sample)
         else:
             z_mean, z_log_var, z = self.encoder(x, training=False)
-        reconstruction = self.decoder(z)
-        reconstruction_loss = keras.losses.MeanSquaredError()(x, reconstruction)
+            reconstruction = self.decoder(z)
+        reconstruction_loss = (1 - self.beta_param) * keras.losses.MeanSquaredError()(x, reconstruction)
         kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
         kl_loss = self.beta_param * tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
         mi_loss = 0
         if self.mi_loss:
-            mi_loss = self.mutual_information_bernoulli_loss(y, z_sample)
+            mi_loss = self.mutual_information_bernoulli_loss(y, z)
         total_loss = reconstruction_loss + kl_loss + self.gamma * mi_loss
         return total_loss.numpy()
 
@@ -481,7 +485,7 @@ class MiVAE(BaseEstimator, keras.Model):
         else:
             z_mean, z_log_var, z = self.encoder(x, training=False)
         reconstruction = self.decoder(z)
-        reconstruction_loss = tf.keras.losses.mse(x, reconstruction)
+        reconstruction_loss = (1 - self.beta_param) * tf.keras.losses.mse(x, reconstruction)
         kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
         kl_loss = self.beta_param * tf.reduce_sum(kl_loss, axis=1)
         total_loss = reconstruction_loss + kl_loss